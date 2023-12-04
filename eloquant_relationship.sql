473b9fc89a1d3b60d3b86aa772cc73675d5444b3
skmirajbn <skmirajbn@gmail.com>
2023-12-04 13:59:13 +0600

    relation_update

--- eloquant_relationship.sql
+++ eloquant_relationship.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 01, 2023 at 09:35 PM
+-- Generation Time: Dec 04, 2023 at 08:52 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -24,12 +24,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `brands`
+--
+
+CREATE TABLE `brands` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `brands`
+--
+
+INSERT INTO `brands` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'Bata', '2023-12-03 18:00:00', '2023-12-03 18:00:00'),
+(2, 'Aarong', '2023-12-03 18:00:00', '2023-12-03 18:00:00');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `comments`
 --
 
 CREATE TABLE `comments` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `post_id` int(11) NOT NULL,
+  `post_id` int(30) NOT NULL,
   `message` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -40,9 +61,59 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`id`, `post_id`, `message`, `created_at`, `updated_at`) VALUES
-(1, 1, 'this is a one to many relation', '2023-12-01 18:00:00', NULL),
-(2, 2, 'Hello Pori', '2023-12-01 18:00:00', '2023-12-01 18:00:00'),
-(3, 0, 'kkk\r\n', '2023-12-01 18:00:00', '2023-12-01 18:00:00');
+(1, 1, 'This is facebook post.we are very excited', '2023-12-03 18:00:00', '2023-12-03 18:00:00'),
+(2, 2, 'This is messenger Post.We can message our collegue.', '2023-12-03 18:00:00', '2023-12-03 18:00:00'),
+(3, 2, 'popular', '2023-12-03 18:00:00', '2023-12-03 18:00:00'),
+(4, 2, 'Very nice', '2023-12-03 18:00:00', '2023-12-03 18:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `customer_name` varchar(255) NOT NULL,
+  `customer_phone` varchar(255) NOT NULL,
+  `customer_address` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`id`, `customer_name`, `customer_phone`, `customer_address`, `created_at`, `updated_at`) VALUES
+(1, 'Rakib', '0126587444', 'Boraishal', NULL, NULL),
+(2, 'Miraj', '012568855', 'Bagerhut', NULL, NULL),
+(3, 'Khaleda', '01269855555', 'Dhaka', NULL, NULL),
+(4, 'Bristy', '01689555555', 'Dhaka', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer_product`
+--
+
+CREATE TABLE `customer_product` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `customer_id` varchar(255) NOT NULL,
+  `product_id` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `customer_product`
+--
+
+INSERT INTO `customer_product` (`id`, `customer_id`, `product_id`, `created_at`, `updated_at`) VALUES
+(1, '1', '1', NULL, NULL),
+(2, '2', '1', NULL, NULL),
+(3, '3', '4', NULL, NULL),
+(4, '4', '5', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -77,13 +148,17 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
-(3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-(5, '2023_12_01_111519_create_phones_table', 1),
-(6, '2023_12_01_195957_create_posts_table', 2),
-(7, '2023_12_01_200909_create_comments_table', 2);
+(8, '2014_10_12_000000_create_users_table', 1),
+(9, '2014_10_12_100000_create_password_reset_tokens_table', 1),
+(10, '2019_08_19_000000_create_failed_jobs_table', 1),
+(11, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(12, '2023_12_01_111519_create_phones_table', 1),
+(13, '2023_12_01_195957_create_posts_table', 1),
+(14, '2023_12_01_200909_create_comments_table', 1),
+(15, '2023_12_04_035919_create_products_table', 2),
+(16, '2023_12_04_035954_create_brands_table', 2),
+(17, '2023_12_04_062530_create_customers_table', 3),
+(18, '2023_12_04_062714_create_customer_product_table', 3);
 
 -- --------------------------------------------------------
 
@@ -125,7 +200,7 @@ CREATE TABLE `personal_access_tokens` (
 CREATE TABLE `phones` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `name` varchar(255) NOT NULL,
-  `user_id` varchar(255) NOT NULL,
+  `usera_id` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -134,9 +209,9 @@ CREATE TABLE `phones` (
 -- Dumping data for table `phones`
 --
 
-INSERT INTO `phones` (`id`, `name`, `user_id`, `created_at`, `updated_at`) VALUES
-(1, 'iphone ', '1', '2023-11-30 18:00:00', '2023-11-30 18:00:00'),
-(2, 'Nokia', '2', '2023-11-30 18:00:00', '2023-11-30 18:00:00');
+INSERT INTO `phones` (`id`, `name`, `usera_id`, `created_at`, `updated_at`) VALUES
+(1, 'iPhone', '1', '2023-12-01 18:00:00', '2023-12-01 18:00:00'),
+(2, 'Nokie', '2', '2023-12-01 18:00:00', '2023-12-01 18:00:00');
 
 -- --------------------------------------------------------
 
@@ -156,8 +231,36 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`id`, `title`, `created_at`, `updated_at`) VALUES
-(1, 'apple is good', '2023-12-01 18:00:00', '2023-12-01 18:00:00'),
-(2, 'title 2', '2023-12-01 18:00:00', '2023-12-01 18:00:00');
+(1, 'Facebook Post', '2023-12-03 18:00:00', '2023-12-03 18:00:00'),
+(2, 'Messenger Post', '2023-12-03 18:00:00', '2023-12-03 18:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `selling_price` varchar(255) NOT NULL,
+  `buying_price` varchar(255) NOT NULL,
+  `brand_id` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `selling_price`, `buying_price`, `brand_id`, `created_at`, `updated_at`) VALUES
+(1, 'Kurty', '4000', '3000', '2', '2023-12-03 18:00:00', '2023-12-03 18:00:00'),
+(2, 'Bag', '3500', '2500', '2', '2023-12-03 18:00:00', '2023-12-03 18:00:00'),
+(3, 'Shoe', '2000', '1000', '1', '2023-12-03 18:00:00', '2023-12-03 18:00:00'),
+(4, 'Jute Bag', '2654', '1562', '1', '2023-12-03 18:00:00', '2023-12-03 18:00:00'),
+(5, 'Sharee', '5000', '3500', '2', '2023-12-03 18:00:00', '2023-12-03 18:00:00'),
+(6, 'Shirt', '6000', '4000', '2', '2023-12-03 18:00:00', '2023-12-03 18:00:00');
 
 -- --------------------------------------------------------
 
@@ -181,17 +284,35 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Rahim', 'rahim@gmail.com', NULL, '123', NULL, '2023-11-30 18:00:00', '2023-11-30 18:00:00'),
-(2, 'Karim', 'karim@gmail.com', NULL, '111', NULL, '2023-12-12 11:26:31', '2023-12-15 11:26:31');
+(1, 'Rahim', 'rahim@gmail.com', NULL, '123', NULL, '2023-12-01 18:00:00', '2023-12-01 18:00:00'),
+(2, 'Karim', 'karim@gmail.com', NULL, '111', NULL, '2023-12-02 06:04:04', '2023-12-01 18:00:00');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `brands`
+--
+ALTER TABLE `brands`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `comments`
 --
 ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `customer_product`
+--
+ALTER TABLE `customer_product`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -234,6 +355,12 @@ ALTER TABLE `posts`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -245,10 +372,28 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `brands`
+--
+ALTER TABLE `brands`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `customer_product`
+--
+ALTER TABLE `customer_product`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -260,7 +405,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -279,6 +424,12 @@ ALTER TABLE `phones`
 --
 ALTER TABLE `posts`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `users`
